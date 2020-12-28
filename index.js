6008a97fe33fccbbc20c84ce8df793dea477beb0
VictoryJu <kob0805@naver.com>
2020-12-28 23:04:42 +0900

    post

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://choi:chltmdwn@db.v4voc.mongodb.net/<dbname>?retryWrites=true&w=majority',
@@ -13,6 +19,24 @@ mongoose.connect('mongodb+srv://choi:chltmdwn@db.v4voc.mongodb.net/<dbname>?retr
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res)=>{
+  
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  //req.body에는 json형식으로 데이터가 담겨있음
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{//mongodb메소드 정보들이 user에 저장됨
+      if(err) return res.json({success: false,err})
+
+      return res.status(200).json({
+        success: true
+      })
+    }) 
+    
 })
 
 app.listen(port, () => {
