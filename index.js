ca3494ca12e45aea86e18a91b8c1bea31a87ee15
VictoryJu <kob0805@naver.com>
2020-12-28 23:45:02 +0900

    몽고DB 정보 보호

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 
+const config = require('./config/key');
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://choi:chltmdwn@db.v4voc.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
 {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected!'))
