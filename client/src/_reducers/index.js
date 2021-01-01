6b4e70ac0c9a78ac7e871d94b6d6447899cdc6e7
VictoryJu <kob0805@naver.com>
2021-01-02 02:09:39 +0900

    combine

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,10 @@
+
+//store에 여러가지 redux들을 root에서 하나로 합쳐준다.
+import { combineReducers} from 'redux';
+//import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    //user.
+})
+
+export default rootReducer;
