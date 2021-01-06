740923f482aac341f60edd1257b02153fbc04149
VictoryJu <kob0805@naver.com>
2021-01-06 18:30:10 +0900

    라우터분리

--- server/models/User.js
+++ server/models/User.js
@@ -37,7 +37,7 @@ const userSchema = mongoose.Schema({
 
 
 userSchema.pre('save', function (next) {
-    var user = this;
+    let user = this;
     if (user.isModified('password')) {
         //비밀번호를 암호화 시킨다.
         bcrypt.genSalt(saltRounds, function (err, salt) {
@@ -65,11 +65,11 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 }
 
 userSchema.methods.generateToken = function (cb) {
-    var user = this;
+    let user = this;
     // console.log('user._id', user._id)
 
     // jsonwebtoken을 이용해서 token을 생성하기 
-    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    let token = jwt.sign(user._id.toHexString(), 'secretToken')
     // user._id + 'secretToken' = token 
     // -> 
     // 'secretToken' -> user._id
