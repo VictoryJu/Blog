cb7c2fe02eae70d0fa46e051bb194efdb6142769
VictoryJu <kob0805@naver.com>
2021-01-06 23:31:39 +0900

    라우터 분리, state작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,42 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
-import LandingPage from './components/views/LandingPage/LandingPage'
-import LoginPage from './components/views/LoginPage/LoginPage'
-import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import Auth from './hoc/auth';
+import Nav from './components/views/NavBar/NavBar';
 
 function App() {
+
+  // useState는 ["value",value를 정정하는 함수]의 형태로 이루어져있다.
+  // ES6의 destructuring문법 
+  // a와b변수에 각각 useState 값을 담는다 
+  // state는 변수대신 쓰는 데이터 저장공간이다.
+  // state는 변경되면 html이 자동으로 재렌더링이 된다.
+  
+
+  
   return (
     <Router>
-    <div>
-      {/*
-        A <Switch> looks through all its children <Route>
-        elements and renders the first one whose path
-        matches the current URL. Use a <Switch> any time
-        you have multiple routes, but you want only one
-        of them to render at a time
-      */}
+      <div>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/main" component={Auth(Nav, true)} />
+        </Switch>
+      </div>
 
-      <Switch>
-        <Route exact path="/" component={Auth(LandingPage,null)} />
-        <Route exact path="/login" component={Auth(LoginPage,false)} />
-        <Route exact path="/register" component={Auth(RegisterPage,false)} />
-      </Switch>
-
-    </div>
-  </Router>
+    </Router>
   );
 }
+
+
 
 export default App;
 
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../../../../src/App.css';
+import { withRouter } from 'react-router-dom';
 
 function NavBar() {
+
+    //state는 value변경 함수를 써야한다.
+    let [title, stitle] = useState(["개발일지","개발일지","개발일지"]);
+    let [like,slike] = useState(0);
+    
+    //array나 object 자료형을 복사할땐 deep copy를 한다.
+    function updatetitle(){
+        // [...array] 서로 독립적인 값을 가지는 복사
+        let newTitle = [...title];
+        newTitle[0] = "일기장";
+        stitle(newTitle)
+    }
+
     return (
-        <div>
-            NavBar
+        <div className="NavBar">
+
+            <div className="black-nav">
+                <div>개발 Blog</div>
+            </div>
+
+            <button onClick={ updatetitle }> 버튼 </button>
+
+            <div className="list">
+                <h3> {title[0]} <span onClick={ ()=>{slike(like+1)} }>👍</span> {like} </h3>
+                <p> 1월 6일 </p>
+                <hr />
+            </div>
+
+            <div className="list">
+                <h3> {title[1]} </h3>
+                <p> 1월 6일 </p>
+                <hr />
+            </div>
+
+            <div className="list">
+                <h3> {title[2]} </h3>
+                <p> 1월 6일 </p>
+                <hr />
+            </div>
+
         </div>
-    )
+    );
 }
 
-export default NavBar
+export default withRouter(NavBar)
